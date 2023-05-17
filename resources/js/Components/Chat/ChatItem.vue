46c57fd7a39ad467f9bbe874d002b8c5d603f5eb
ISNewton <ashrfalhag.11@gmail.com>
2023-05-17 14:56:16 +0200

    seprate chat items

---
+++ resources/js/Components/Chat/ChatItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+<div
+          class="flex flex-row py-4 px-2 items-center border-b-2 border-l-4 border-blue-400"
+        >
+          <div class="w-1/4">
+            <img
+              src="https://source.unsplash.com/L2cxSuKWbpo/600x600"
+              class="object-cover h-12 w-12 rounded-full"
+              alt=""
+            />
+          </div>
+          <div class="w-full">
+            <div class="text-lg font-semibold">MERN Stack</div>
+            <span class="text-gray-500">Lusi : Thanks Everyone</span>
+          </div>
+        </div>
+</template>
